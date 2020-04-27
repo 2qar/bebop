c878960b1f9850203b2733542f01fb0517c8dd26
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2020-04-26 21:09:01 -0700

    use async_stdin so input doesn't block

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() -> Result<(), io::Error> {
     let mut player = Player::new(0.2);
 
     let stdout = io::stdout().into_raw_mode()?;
+    let mut stdin = termion::async_stdin().events();
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
@@ -46,51 +47,61 @@ fn main() -> Result<(), io::Error> {
                 .block(Block::default().title("Artists").borders(Borders::ALL))
                 .highlight_style(Style::default().bg(Color::Green).modifier(Modifier::BOLD));
             f.render_stateful_widget(block, chunks[0], explorer.list_state());
+
         })?;
 
         player.advance_if_empty()?;
 
-        // FIXME make it less panicky looking
-        match io::stdin().events().next().unwrap().unwrap() {
-            Event::Key(Key::Char('q')) => {
-                // TODO: use termion alternate screen to preserve the terminal's state prior to
-                //       opening this
-                terminal.clear()?;
-                return Ok(());
+        let key = match stdin.next() {
+            Some(r) => match r {
+                Ok(c) => Some(c),
+                Err(_) => None,
             },
-            Event::Key(Key::Char('j')) => {
-                explorer.select_next();
-            },
-            Event::Key(Key::Char('k')) => {
-                explorer.select_previous();
-            },
-            Event::Key(Key::Char('h')) => {
-                explorer.select_previous_dir();
-            },
-            Event::Key(Key::Char('l')) => {
-                explorer.select_next_dir()?;
-            },
-            Event::Key(Key::Char('g')) => {
-                explorer.top();
-            },
-            Event::Key(Key::Char('G')) => {
-                explorer.bottom();
-            },
-            Event::Key(Key::Char('\n')) => {
-                match explorer.state() {
-                    State::Songs => {
-                        player.play_file(explorer.selected_dir().selected().path())?;
-                    },
-                    State::Albums => {
-                        player.play_album(explorer.selected_dir().selected().path())?;
-                    },
-                    _ => (),
-                }
-            },
-            Event::Key(Key::Char('p')) => {
-                player.toggle_pause()
-            },
-            _ => (),
+            None => None,
+        };
+
+        if let Some(c) = key {
+            match c {
+                Event::Key(Key::Char('q')) => {
+                    // TODO: use termion alternate screen to preserve the terminal's state prior to
+                    //       opening this
+                    terminal.clear()?;
+                    return Ok(());
+                },
+                Event::Key(Key::Char('j')) => {
+                    explorer.select_next();
+                },
+                Event::Key(Key::Char('k')) => {
+                    explorer.select_previous();
+                },
+                Event::Key(Key::Char('h')) => {
+                    explorer.select_previous_dir();
+                },
+                Event::Key(Key::Char('l')) => {
+                    explorer.select_next_dir()?;
+                },
+                Event::Key(Key::Char('g')) => {
+                    explorer.top();
+                },
+                Event::Key(Key::Char('G')) => {
+                    explorer.bottom();
+                },
+                Event::Key(Key::Char('\n')) => {
+                    match explorer.state() {
+                        State::Songs => {
+                            player.play_file(explorer.selected_dir().selected().path())?;
+                        },
+                        State::Albums => {
+                            player.play_album(explorer.selected_dir().selected().path())?;
+                        },
+                        _ => (),
+                    }
+                },
+                Event::Key(Key::Char('p')) => {
+                    player.toggle_pause()
+                },
+                _ => (),
+            }
         }
     }
 }
